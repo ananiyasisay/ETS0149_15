26561957a22d4845fc0fd57461fc43cbd44411f5
ananiyasisay <ananiyasisay2003@gmail.com>
2025-03-24 10:55:47 -0700

    test 02

---
+++ python/test02.py
@@ -0,0 +1,8 @@
+word = "i would like to be a pilot"
+word1 = str.capitalize(word)
+print(word1)
+print(word)
+print(word.capitalize())
+print(word.title())
+assign ="I WIll bE millioner"
+print (assign.swapcase())
