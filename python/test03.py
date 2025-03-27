6d3a5e3222eec4a75ec752c8e35441c77c02dda4
ananiyasisay <ananiyasisay2003@gmail.com>
2025-03-27 05:39:14 -0700

    test 3

---
+++ python/test03.py
@@ -0,0 +1,10 @@
+assign = "I WANT to BE Billioner"
+print (assign.lower())
+
+word = "Hey Ethiopia"
+print (word.find("Hey"))
+print (word.find("Bye"))
+
+print (word.index("Ethiopia"))
+print (word.index("Egypt"))
+
