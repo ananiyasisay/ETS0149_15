81f8740cf8269dcc00c4787a78106f00e0f570db
ananiyasisay <ananiyasisay2003@gmail.com>
2025-03-29 08:18:07 -0700

    test04

---
+++ python/test04.py
@@ -0,0 +1,11 @@
+text = "I will be a programer as well as i wanna be a millioner"
+print (text.startswith("I"))
+print (text.startswith("be"))
+
+text2 = "Hey Ethiopia i will always be there for you"
+print(text2.endswith("you"))
+print(text2.endswith("Hey"))
+
+text3 = "I will be a programer as well as i wanna be a millioner one day"
+print(text3.count("as"))
+print(text3.count("I"))
