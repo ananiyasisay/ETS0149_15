e330b7e80600447a17a08f4157b730ae16b54309
ananiyasisay <ananiyasisay2003@gmail.com>
2025-03-30 06:59:43 -0700

    test 05

---
+++ python/test05.py
@@ -0,0 +1,11 @@
+text ="Hello Ethiopia Hello Africa"
+print(text.replace("Hello","Hey"))
+# if want to replace only one substring we can write 1 after what we wanna replace
+print(text.replace("Hello","Hey",1))
+
+text1 ="mmmmi Love You"
+print(text1.strip("m"))
+
+text2 ="mmmmi Love you    mmmmm"
+print(text2.mstrip(''))
+
