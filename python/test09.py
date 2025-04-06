c5529c9d2bfa2304a1e641099e9c409687d3a513
ananiyasisay <ananiyasisay2003@gmail.com>
2025-04-06 13:08:49 -0700

    test09

---
+++ python/test09.py
@@ -0,0 +1,23 @@
+append()
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.append('orange')
+
+print(fruits)  # Output: ['apple', 'banana', 'cherry', 'orange']
+
+ clear()
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.clear()
+
+print(numbers)  # Output: []
+
+copy()
+
+original_list = [1, 2, 3]
+
+copied_list = original_list.copy()
+
+print(copied_list)  # Output: [1, 2, 3]
